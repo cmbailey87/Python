bb283e97b9a8d1aece6a261d1abac3fdd93bb360
Christopher Bailey <ARCTRAVEL\cbailey@a-2bw753m2o3t98.arc.travel>
2020-02-06 09:18:12 -0500

    added encryption

--- encryptPW.py
+++ encryptPW.py
@@ -7,17 +7,21 @@ from cryptography.fernet import Fernet
 #key = b'pRmgMa8T0INjEAfksaq2aafzoZXEuwKI7wDe4c1F8AY='
 key = b'Iixkr_C_Q8XbU3FoqfIgshj9d-Vnf3-cUMa9ThzwY-8='
 cipher_suite = Fernet(key)
-ciphered_text = cipher_suite.encrypt(b'Brotatos2100')   #required to be bytes
+ciphered_text = cipher_suite.encrypt(b'J0J0BizarreAD')   #required to be bytes
 print(ciphered_text)
 
 
 #ciphered_text = b'gAAAAABaHvk3g8IG4cln7g5HCulppy1bAPVuhtskVcgPXRyytx6RkIqjcI0mAMA7Oy_56T6J0dk-yjxI_WlZtjxnUBbR-EvoQa_oqCKoQJFbv_uc2WdXMSI='
 #key = b'pRmgMa8T0INjEAfksaq2aafzoZXEuwKI7wDe4c1F8AY='
 
+
 from cryptography.fernet import Fernet
-key = b'Iixkr_C_Q8XbU3FoqfIgshj9d-Vnf3-cUMa9ThzwY-8='
-cipher_suite = Fernet(key)
-ciphered_text = b'gAAAAABd8U9JFy8zj28dtuN5iYutHpOzBd9Ij74ZcSH16TKbUzomQYcCqBWuVabjFzzbEiMGc3DMmaBDpCu2HOgUwa2XWLCRaA=='
-unciphered_text = (cipher_suite.decrypt(ciphered_text))
+def pw_decrypter():
+    key = b'Iixkr_C_Q8XbU3FoqfIgshj9d-Vnf3-cUMa9ThzwY-8='
+    cipher_suite = Fernet(key)
+    ciphered_text = b'gAAAAABd_S9TcWwZw0UpRH_GLHWd8KNcLLkmfLD-1-U1E0_XKqOeyB4Z8sIvvAXO27x4DmW79QZL4RteX6F8OQJLBR89jDCJjQ=='
+    unciphered_text = (cipher_suite.decrypt(ciphered_text))
+    pwd = str(unciphered_text)[2:15]
+    return pwd
+
 print(unciphered_text)
-pwd = str(unciphered_text)[2:14]
